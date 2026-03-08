dd8404fdd77a069efeac294ad8f8d71c1839e7dd
Rai Yashasvee Srivastav <rai.srivastav@hotmail.com>
2026-03-08 16:03:00 +0530

    Hyper Parameter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -10,6 +10,7 @@ from sklearn.ensemble import (
 )
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+from sklearn.base import BaseEstimator
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
@@ -17,6 +18,10 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object, evaluate_models
+
+class CustomCatBoostRegressor(BaseEstimator, CatBoostRegressor):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 @dataclass
 class ModelTrainerConfig:
@@ -44,11 +49,55 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 "K-Neighbours Regressor": KNeighborsRegressor(),
                 "XGBRegressor": XGBRegressor(),
-                "Catboosting Regressor": CatBoostRegressor(verbose=False),
+                "Catboosting Regressor": CustomCatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }                
+            }
+
+            model_report:dict = evaluate_models(
+                X_train=X_train,
+                y_train=y_train,
+                X_test=X_test,
+                y_test=y_test,
+                models=models,
+                params=params
+            )
 
             ## To get best model score from dictionary
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,18 +20,28 @@ def save_object(file_path, obj):
     except Exception as ex:
         raise CustomException(ex, sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
+        # iterate through each model and optionally use corresponding params
         for model_name, model in models.items():
-            model.fit(X_train, y_train) #Train Model
+            # safely fetch parameters for this model (may be empty)
+            model_params = params.get(model_name, {})
+            # if you later want to perform grid search or set params, use model_params
+
+            gs = GridSearchCV(model,model_params,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+
+            #model.fit(X_train, y_train)  # Train Model
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[model_name] = test_model_score
